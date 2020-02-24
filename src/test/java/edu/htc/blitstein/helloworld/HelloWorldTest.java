dcdebf659503a7d074fa4e49bb7df4e6ffa739f2
Michael Allen <michael.allen@htc.edu>
2020-02-24 13:09:48 -0600

    added testing input and output to files added expecting file not found exception

--- src/test/java/edu/htc/blitstein/helloworld/HelloWorldTest.java
+++ src/test/java/edu/htc/blitstein/helloworld/HelloWorldTest.java
@@ -3,6 +3,9 @@ package edu.htc.blitstein.helloworld;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.*;
+import java.util.Scanner;
+
 import static org.junit.Assert.*;
 
 public class HelloWorldTest {
@@ -21,4 +24,37 @@ public class HelloWorldTest {
         assertEquals(expectedMessage, actualName);
     }
 
+    @Test
+    public void testOutputFileNormal() throws IOException {
+        File file = new File("src/test/resources/testfile.txt");
+        PrintStream printStream = new PrintStream(file);
+        helloWorld = new HelloWorld(printStream);
+        helloWorld.printGreeting("George");
+        printStream.close();
+        String expectedLine = "Hello, George!";
+        InputStream inputStream = new FileInputStream(file);
+        Scanner inFile = new Scanner(inputStream);
+        String actualLine = inFile.nextLine();
+        assertEquals(expectedLine, actualLine);
+        inputStream.close();
+        boolean isDeleted = file.delete();
+        assertTrue(isDeleted);
+    }
+
+    @Test(expected = FileNotFoundException.class)
+    public void testOutputFileNotFound() throws IOException {
+        File file = new File("blah/testfile.txt");
+        PrintStream printStream = new PrintStream(file);
+        helloWorld = new HelloWorld(printStream);
+        helloWorld.printGreeting("George");
+        printStream.close();
+        String expectedLine = "Hello, George!";
+        InputStream inputStream = new FileInputStream(file);
+        Scanner inFile = new Scanner(inputStream);
+        String actualLine = inFile.nextLine();
+        assertEquals(expectedLine, actualLine);
+        inputStream.close();
+        boolean isDeleted = file.delete();
+        assertTrue(isDeleted);
+    }
 }
